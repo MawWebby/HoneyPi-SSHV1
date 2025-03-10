fbc3dd215e6c8f7447cdcdfd17d897466b78a222
MawWebby <mawhitworth01@gmail.com>
2025-03-10 09:48:51 -0500

    Update Virtual Terminal

--- virtualterminal.cpp
+++ virtualterminal.cpp
@@ -40,6 +40,7 @@ void virtualterminal(std::string command, int method) {
     int writeback = open(sshfifo.c_str(), O_WRONLY);
     std::string commandpost = "";
     bool foundcommand = false;
+    bool validflags = true;
 
 
     // MAIN EXECUTION OF DIFFERENT FAKE COMMANDS
@@ -51,6 +52,7 @@ void virtualterminal(std::string command, int method) {
         std::string firstfive = "";
         std::string firstsix = "";
         std::string firsteight = "";
+        compilepacket(command, 7);
         if (command.length() >= 8) {
             firsteight = command.substr(0,8);
             firstsix = command.substr(0,6);
@@ -113,7 +115,10 @@ void virtualterminal(std::string command, int method) {
 
 
         // ***** l
-
+            // ls
+            
+            // logout
+            
 
         // ***** m
             // mkdir
@@ -150,7 +155,11 @@ void virtualterminal(std::string command, int method) {
         // ***** u
             // uname
             if (firstfive == "uname") {
+                if (command.length() == 5) {
+                    commandpost = "Linux";
+                } else {
 
+                }
             }
 
         // ***** v
@@ -174,7 +183,7 @@ void virtualterminal(std::string command, int method) {
         // TEMP NO FOUND //
         ///////////////////
         if (foundcommand != true) {
-            commandpost = "bash: " + command + ": command not found";
+            commandpost = "\r\n-bash: " + command + ": command not found\r";
         }
 
         write(writeback, commandpost.c_str(), commandpost.length());
@@ -195,7 +204,7 @@ void readback() {
     showinput = true;
 
 
-    std::string userprompt = "\npi@server:~/Documents";
+    std::string userprompt = "\npi@server:~/ $ ";
     // FIX THIS TO ACCURATELY PORTRAY INFORMATION ALONG WITH TERMINAL AND BRUTE FORCE SSH
 
     // FOREVER LOOP TO HAVE CONSTANT READER
@@ -204,6 +213,7 @@ void readback() {
         sleep(0.1);
 
         char whyyy[100];
+        //int bytes = 0;
         int bytes = read(fd, whyyy, 100);
 
         std::cout << "BYTES READ: " << bytes << std::endl;
@@ -214,7 +224,7 @@ void readback() {
             if (readfromfifo.substr(0,bytes) != "") {
                 //std::cout << "CMD PIPE: " << whyyy << std::endl;
                 //std::cout << "ASCII VALUE: " << (int)whyyy[0] << std::endl;
-
+                
                 // SHOW INPUT ON MASTER DISPLAY
                 if (showinput == true) {
                     write(writeback, readfromfifo.c_str(), readfromfifo.length());
@@ -248,11 +258,13 @@ void sshwriter() {
     char arr1[80], arr2[80];
     std::map<int, std::string> mappymap;
     int fd = open(sshfifo.c_str(), O_WRONLY);
-    //std::string hellomessage = "Linux Server 6.6.74+rpt-rpi-v7 #1 SMP Raspbian 1:6.6.74-1+rpt1 (2025-01-27) armv7l\n\nThe programs included with the Debian GNU/Linux system are free software; \nthe exact distribution terms for each program are described in the \nindividual files in /usr/share/doc/*/copyright. \n\n Debian GNU/Linux comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law. Last login: " + time(0);
+    time_t time10 = time(0); 
+    std::string dateandtime = ctime(&time10);
+    std::string hellomessage = "Linux Server 6.6.74+rpt-rpi-v7 #1 SMP Raspbian 1:6.6.74-1+rpt1 (2025-01-27) armv7l\r\n\r\nThe programs included with the Debian GNU/Linux system are free software;\r\nthe exact distribution terms for each program are described in the\r\nindividual files in /usr/share/doc/*/copyright.\r\n\r\nDebian GNU/Linux comes with ABSOLUTELY NO WARRANTY, to the extent\r\npermitted by applicable law. Last login: " + dateandtime.substr(0, dateandtime.length() - 1) + " from " + "FIX THIS\r\n";
     int cached = 0;
 
     // userprompt fix this
-    std::string userprompt = "pi@server:~/Documents";
+    std::string userprompt = "pi@server:~/ $ ";
     // FIX THIS TO ACCURATELY PORTRAY INFORMATION ALONG WITH TERMINAL AND BRUTE FORCE SSH
 
     
@@ -261,11 +273,11 @@ void sshwriter() {
     //write(fd, hellomessage.c_str(), hellomessage.length());
     //std::cout << "CLOSEFD1" << std::endl;
 
-    sleep(1);
+    sleep(2);
     
-    //write(fd, hellomessage.c_str(), hellomessage.length());
+    write(fd, hellomessage.c_str(), hellomessage.length());
         
-    sleep(3);
+    sleep(2);
     
     write(fd, userprompt.c_str(), userprompt.length());
     
