6c97cfd47a938a8006059cd57954b82e2836fa20
MawWebby <mawhitworth01@gmail.com>
2025-04-01 09:33:14 -0500

    Change for Localization

--- virtualterminal.h
+++ virtualterminal.h
@@ -5,13 +5,13 @@
 #ifndef VIRTUALTERMINAL_H
 #define VIRTUALTERMINAL_H
 
+// USER SCRIPT
+std::string userfunction();
+
 // MAIN VIRTUAL TERMINAL THREAD
 void virtualterminal(std::string, int);
 
 // MAIN READBACK FUNCTION FROM SSH
 void readback();
-
-// MAIN SENDING FUNCTION TO SSH
-void sshwriter();
 
 #endif
